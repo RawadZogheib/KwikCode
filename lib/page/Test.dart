9986d6d7d2e8726c0ff04d91e3a152274f34be0f
zogheibRawad <89975932+Toska003@users.noreply.github.com>
2022-01-16 15:19:08 +0200

    .

--- lib/page/Test.dart
+++ lib/page/Test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Test extends StatefulWidget {
@@ -9,7 +11,31 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //_timer();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Image(
+        image: AssetImage('Assets/3Dobjects/Comet.gif'),
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+      ),
+    );
+  }
+
+  _timer() async {
+    try {
+      Future.delayed(const Duration(seconds: 3), () {
+        Navigator.pushNamedAndRemoveUntil(context, '/Login', (route) => false);
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
