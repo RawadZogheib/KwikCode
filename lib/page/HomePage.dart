e6a6b90dfee382841b57f2970001b27fda14dbc8
zogheibRawad <89975932+Toska003@users.noreply.github.com>
2022-02-08 20:32:00 +0200

    .

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:kwikcode/globals/globals.dart' as globals;
 import 'package:kwikcode/hexColor/hexColor.dart';
@@ -10,25 +13,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double _dist = 0;
+  int k = 0;
+  int min = Random().nextInt(3) + 2;
+  int max = Random().nextInt(7) + 18;
+  bool play = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    distAnimation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor('#222222'),
+      backgroundColor: HexColor('#101010'),
       body: Column(
         children: [
           Container(
-            height: MediaQuery.of(context).size.height * 0.12,
-            color: Colors.transparent,
+            height: MediaQuery.of(context).size.height * 0.15,
+            decoration: BoxDecoration(
+              color: HexColor('#222222'),
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(12.0),
+                bottomLeft: Radius.circular(12.0),
+              ),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Row(
                   children: [
                     Image.asset(
-                      'Assets/3Dobjects/KwikCode.png',
-                      fit: BoxFit.cover,
-                      height: MediaQuery.of(context).size.height * 0.09,
-                      width: MediaQuery.of(context).size.width * 0.09,
+                      'Assets/3Dobjects/KwikCodeLogo.png',
+                      fit: BoxFit.contain,
+                      height: MediaQuery.of(context).size.height * 0.12,
+                      width: MediaQuery.of(context).size.width * 0.12,
                     ),
                     Text(
                       'KwikCode',
@@ -39,6 +61,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
+                // Buttons
                 Expanded(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -54,11 +77,10 @@ class _HomePageState extends State<HomePage> {
                               alignment: Alignment.center,
                               padding: const EdgeInsets.all(9.0),
                               decoration: BoxDecoration(
-                                color: HexColor('#8848f7'),
+                                color: HexColor('#cb156f'),
                                 borderRadius: const BorderRadius.only(
-                                  bottomRight: Radius.circular(22.0),
-                                  topLeft: Radius.circular(22.0)
-                                ),
+                                    bottomRight: Radius.circular(22.0),
+                                    topLeft: Radius.circular(22.0)),
                               ),
                               child: const Text(
                                 'Home',
@@ -78,8 +100,7 @@ class _HomePageState extends State<HomePage> {
                                 color: Colors.white,
                                 borderRadius: BorderRadius.only(
                                     bottomRight: Radius.circular(22.0),
-                                    topLeft: Radius.circular(22.0)
-                                ),
+                                    topLeft: Radius.circular(22.0)),
                               ),
                               child: Text(
                                 'Services',
@@ -98,8 +119,8 @@ class _HomePageState extends State<HomePage> {
                               decoration: const BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.only(
-                                    bottomRight: Radius.circular(22.0),
-                                    topLeft: Radius.circular(22.0)
+                                  bottomRight: Radius.circular(22.0),
+                                  topLeft: Radius.circular(22.0),
                                 ),
                               ),
                               child: Text(
@@ -120,8 +141,7 @@ class _HomePageState extends State<HomePage> {
                                 color: Colors.white,
                                 borderRadius: BorderRadius.only(
                                     bottomRight: Radius.circular(22.0),
-                                    topLeft: Radius.circular(22.0)
-                                ),
+                                    topLeft: Radius.circular(22.0)),
                               ),
                               child: Text(
                                 'About',
@@ -141,58 +161,208 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
+          SizedBox(
+            height: _dist,
+          ),
           Expanded(
-            child: ListView(
-              children: [
-                Stack(
-                  children: [
-                    Image.asset(
-                      'Assets/HomePage/background.jpg',
-                      fit: BoxFit.cover,
-                      height: MediaQuery.of(context).size.height * 0.88,
-                      width: MediaQuery.of(context).size.width * 1,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(32.0),
+                topLeft: Radius.circular(32.0),
+              ),
+              child: ListView(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(32.0),
+                      topLeft: Radius.circular(32.0),
+                      bottomRight: Radius.circular(32.0),
+                      bottomLeft: Radius.circular(32.0),
                     ),
-                    Container(
-                      color: Colors.transparent,
-                      height: MediaQuery.of(context).size.height * 0.88,
-                      width: MediaQuery.of(context).size.width * 1,
-                      padding: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.width * 0.05,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '  Build a \n     Kwik Code',
-                            style: TextStyle(
-                                color: globals.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 90),
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          'Assets/HomePage/background.jpg',
+                          fit: BoxFit.cover,
+                          height: MediaQuery.of(context).size.height * 0.83,
+                          width: MediaQuery.of(context).size.width * 1,
+                        ),
+                        Container(
+                          color: Colors.transparent,
+                          height: MediaQuery.of(context).size.height * 0.83,
+                          width: MediaQuery.of(context).size.width * 1,
+                          padding: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width * 0.05,
                           ),
-                          const Text(
-                            ' Computer science involves the study of computation, automation,\n'
-                            ' and information. Computer science spanstheoretical disciplines\n '
-                            '(such as algorithms, theory of computation, and information theory) \n'
-                            ' to practical disciplines (including the design and implementation of\n '
-                            'hardware and software). Computer science is generally considered\n'
-                            ' [by whom?] an area of academic researchs and distinct from computer \n'
-                            ' programming.\n',
-                            style: TextStyle(color: Colors.grey, fontSize: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '  Build a \n     Kwik Code',
+                                style: TextStyle(
+                                    color: globals.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 90),
+                              ),
+                              const Text(
+                                ' Computer science involves the study of computation, automation,\n'
+                                ' and information. Computer science spanstheoretical disciplines\n '
+                                '(such as algorithms, theory of computation, and information theory) \n'
+                                ' to practical disciplines (including the design and implementation of\n '
+                                'hardware and software). Computer science is generally considered\n'
+                                ' [by whom?] an area of academic researchs and distinct from computer \n'
+                                ' programming.\n',
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 20),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 2000,
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: _dist,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.82,
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(32.0),
+                            topLeft: Radius.circular(32.0),
+                            bottomRight: Radius.circular(32.0),
+                            bottomLeft: Radius.circular(32.0),
+                          ),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.82,
+                            width: MediaQuery.of(context).size.width * 0.49,
+                            color: HexColor('#333333'),
+                          ),
+                        ),
+                        SizedBox(
+                          width: _dist,
+                        ),
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(32.0),
+                            topLeft: Radius.circular(32.0),
+                            bottomRight: Radius.circular(32.0),
+                            bottomLeft: Radius.circular(32.0),
+                          ),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.82,
+                            width: MediaQuery.of(context).size.width * 0.49,
+                            color: HexColor('#333333'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: _dist,
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(32.0),
+                      topLeft: Radius.circular(32.0),
+                      bottomRight: Radius.circular(32.0),
+                      bottomLeft: Radius.circular(32.0),
+                    ),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.82,
+                      width: MediaQuery.of(context).size.width * 1,
+                      color: HexColor('#ffffff'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: _dist,
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(32.0),
+                      topLeft: Radius.circular(32.0),
+                      bottomRight: Radius.circular(32.0),
+                      bottomLeft: Radius.circular(32.0),
+                    ),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.82,
+                      width: MediaQuery.of(context).size.width * 1,
+                      color: HexColor('#cb156f'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: _dist,
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(32.0),
+                      topLeft: Radius.circular(32.0),
+                      bottomRight: Radius.circular(32.0),
+                      bottomLeft: Radius.circular(32.0),
+                    ),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.82,
+                      width: MediaQuery.of(context).size.width * 1,
+                      color: HexColor('#222222'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 2000,
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> distAnimation() async {
+    Timer timer =
+        Timer.periodic(const Duration(milliseconds: 110), (Timer t) async {
+      // while (play != false) {
+      //   await Future.delayed(const Duration(milliseconds: 100));
+      print("110msec gone!!");
+      if (k % 2 == 0) {
+        if (mounted) {
+          setState(() {
+            _dist++;
+          });
+        }
+        if (_dist == max) {
+          k++;
+          min = Random().nextInt(3) + 2;
+          //
+          print(min);
+          //
+          // play = false;
+          // await Future.delayed(const Duration(milliseconds: 200));
+          // play = false;
+        }
+      } else {
+        if (mounted) {
+          setState(() {
+            _dist--;
+          });
+        }
+        if (_dist == min) {
+          k++;
+          max = Random().nextInt(7) + 18;
+          //
+          print(max);
+          //
+          // play = false;
+          // await Future.delayed(const Duration(milliseconds: 200));
+          // play = false;
+        }
+        //}
+      }
+    });
   }
 }
